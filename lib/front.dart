3ab601696e0e6c2c6b96d77d6da34e11df29ef6f
shenoyganesprasad <shenoyganeshprasad@gmail.com>
2019-12-28 14:12:13 +0530

    add front side

---
+++ lib/front.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FrontSide extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 50.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: Text(
+                'Ganeshprasad Shenoy',
+                style: GoogleFonts.roboto(
+                  textStyle: TextStyle(fontSize: 25.0, color: Colors.black),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Image(
+                image: AssetImage('assets/ganesh.jpg'),
+                fit: BoxFit.contain,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'front.dart';
 import 'package:flip_card/flip_card.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: FlipCard(
+          front: FrontSide(),
+          back: Container(
+            child: Text('BACK'),
+          ),
+        ),
+      ),
+    );
   }
 }
