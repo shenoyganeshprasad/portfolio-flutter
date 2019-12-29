a6b3b94b6dfe9ecb1eb2d4b551e1fb3b27e390bb
shenoyganesprasad <shenoyganeshprasad@gmail.com>
2019-12-29 20:42:59 +0530

    update homepage

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -8,9 +8,11 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: FlipCard(
-          front: FrontSide(),
-          back: BackSide(),
+        child: Container(
+          child: FlipCard(
+            front: FrontSide(),
+            back: BackSide(),
+          ),
         ),
       ),
     );
