382651447d715c4b57f6e835e4da62e3096ff330
shenoyganesprasad <shenoyganeshprasad@gmail.com>
2019-12-29 20:42:11 +0530

    change front UI

--- lib/front.dart
+++ lib/front.dart
@@ -1,31 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'widgets/buttons.dart';
 
 class FrontSide extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 50.0),
-        child: Column(
+    return Center(
+      child: Container(
+        height: 300.0,
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          // mainAxisSize: MainAxisSize.max,
           children: <Widget>[
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 10.0),
-              child: Text(
-                'Ganeshprasad Shenoy',
-                style: GoogleFonts.roboto(
-                  textStyle: TextStyle(fontSize: 25.0, color: Colors.black),
+            Container(
+              width: 150.0,
+              height: 150.0,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 5.0,
+                    spreadRadius: 2.0,
+                  )
+                ],
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/ganesh.jpg'),
                 ),
               ),
             ),
-            Expanded(
-              child: Image(
-                image: AssetImage('assets/ganesh.jpg'),
-                fit: BoxFit.contain,
-              ),
+            SizedBox(width: 20.0),
+            VerticalDivider(),
+            SizedBox(width: 20.0),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Ganeshprasad Shenoy',
+                  style: GoogleFonts.roboto(
+                    textStyle: TextStyle(
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Row(
+                  children: <Widget>[
+                    Buttons(
+                        image: AssetImage('assets/github.png'),
+                        websiteName: 'Github',
+                        websiteLink: 'http://bit.ly/32Sta8L',
+                        colour: Color(0xFF5C6BC0)),
+                    SizedBox(width: 8.0),
+                    Buttons(
+                        image: AssetImage('assets/linkedin.png'),
+                        websiteName: 'LinkedIn',
+                        websiteLink: 'http://bit.ly/33QXxhe',
+                        colour: Color(0xFF2076D2)),
+                  ],
+                ),
+                SizedBox(height: 8.0),
+                Row(
+                  children: <Widget>[
+                    Buttons(
+                        image: AssetImage('assets/facebook.png'),
+                        websiteName: 'Facebook',
+                        websiteLink: 'http://bit.ly/33ZILow',
+                        colour: Color(0xFF2076D2)),
+                    SizedBox(width: 8.0),
+                    Buttons(
+                        image: AssetImage('assets/instagram.png'),
+                        websiteName: 'Instagram',
+                        websiteLink: 'http://bit.ly/2XkxRqz',
+                        colour: Color(0xFFEC4041)),
+                    SizedBox(width: 8.0),
+                    Buttons(
+                        image: AssetImage('assets/twitter.png'),
+                        websiteName: 'Twitter',
+                        websiteLink: 'http://bit.ly/2NRb4zC',
+                        colour: Color(0xFF3EAAF5)),
+                    SizedBox(width: 8.0),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
